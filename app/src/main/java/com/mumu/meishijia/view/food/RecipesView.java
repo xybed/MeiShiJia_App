160636e2c97ac39453a321d5eda8e0f68f92d418
liqi <940608848@qq.com>
2016-12-14 15:43:32 +0800

    增加流式布局、frameProgress，建好文件夹，预备写菜谱分类的请求

---
+++ app/src/main/java/com/mumu/meishijia/view/food/RecipesView.java
@@ -0,0 +1,12 @@
+package com.mumu.meishijia.view.food;
+
+import com.mumu.meishijia.view.BaseView;
+
+/**
+ * Created by 7mu on 2016/12/14.
+ * 食谱view接口
+ */
+
+public interface RecipesView extends BaseView{
+
+}
